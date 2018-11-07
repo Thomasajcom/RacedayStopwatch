2decb9d34bd07d4f3d526d153817374d0a878901
Thomas A Johansen <thomasaj@gmail.com>
2018-11-07 18:35:26 +0100

    Initial Commit

---
+++ RacedayStopwatch/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RacedayStopwatch
+//
+//  Created by Thomas Andre Johansen on 07/11/2018.
+//  Copyright © 2018 Appbryggeriet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RacedayStopwatch/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RacedayStopwatch
+//
+//  Created by Thomas Andre Johansen on 07/11/2018.
+//  Copyright © 2018 Appbryggeriet. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
